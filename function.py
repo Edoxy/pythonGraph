e6eb0301cc0ef8065ecbacdc6baf5929d1c691da
EDOARDO VAY <S258053@studenti.polito.it>
2020-09-04 09:26:01 +0200

    load

--- function.py
+++ function.py
@@ -3,7 +3,7 @@ import graphs
 def load(percorso_file):
     '''Aggiunge il grafo G dalla cartella'''
 
-    graph = DirectedGraph()
+    graph = graphs.DirectedGraph()
     graph.add_from_files(percorso_file)
     
     return graph
